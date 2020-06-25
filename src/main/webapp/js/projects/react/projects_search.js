1e8b0a4a01888a7c527e33b6f62d7146321b0bf5
Richie Goulazian <rgoulazian@gmail.com>
2020-06-25 18:30:02 -0400

    Add projects page renderer

---
+++ src/main/webapp/js/projects/react/projects_search.js
@@ -0,0 +1,4 @@
+import ProjectList from './components/ProjectList.js';
+
+const projectsContainer = document.getElementById("projects-container");
+ReactDOM.render(<ProjectList />, projectsContainer);
