f4cf7892b0d8a7b4028559688978a88c63190967
Richie Goulazian <rgoulazian@google.com>
2020-06-25 15:19:18 +0000

    Break lines to stay within col limit

--- src/main/webapp/js/common/react/Navbar.js
+++ src/main/webapp/js/common/react/Navbar.js
@@ -13,12 +13,20 @@ function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">Open Sesame</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          {urls.map((url, i) => <NavbarLink key={i} href={url.href} name={url.name} />)}
+          {urls.map((url, i) => 
+              <NavbarLink key={i} href={url.href} name={url.name} />)}
         </ul>
       </div>
     </nav>
