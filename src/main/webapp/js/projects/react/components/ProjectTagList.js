d131430ce8a43c238a04dccd34c9d86351c0803c
Richie Goulazian <rgoulazian@gmail.com>
2020-06-25 20:24:20 -0400

    Remove trailing spaces

--- src/main/webapp/js/projects/react/components/ProjectTagList.js
+++ src/main/webapp/js/projects/react/components/ProjectTagList.js
@@ -1,7 +1,7 @@
 /**
  * A horizontal list of badge-based tags for a project.
  * @param {{tags: ProjectTag[]}} props
- * @return {React.Component} Returns a React component list of tags for a 
+ * @return {React.Component} Returns a React component list of tags for a
  * project.
  */
 export default function ProjectTagList(props) {
@@ -9,7 +9,7 @@ export default function ProjectTagList(props) {
     <div className="d-flex flex-wrap">
       {props.tags.map((tag, i) => {
         return (
-          <div key={i} 
+          <div key={i}
               className="border border-muted text-muted mr-1 mb-1 badge">
             {tag.text}
           </div>
