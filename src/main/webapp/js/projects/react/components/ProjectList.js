a0fb30f62617f21b91a96a51a6a98d036e2f05f4
Richie Goulazian <rgoulazian@google.com>
2020-06-25 20:21:22 -0400

    Remove trailing spaces from ProjectPreview.js

--- src/main/webapp/js/projects/react/components/ProjectList.js
+++ src/main/webapp/js/projects/react/components/ProjectList.js
@@ -1,28 +1,28 @@
 import ProjectPreview from './ProjectPreview.js';
 
-// ProjectTag is an object and not a string because of the upcoming 
+// ProjectTag is an object and not a string because of the upcoming
 // addition of different types of tags (to be indicated with a tagType property)
 /**
  * @typedef {Object} ProjectTag
- * @property {string} text 
+ * @property {string} text
  */
 /**
  * @typedef {Object} ProjectPreviewData
  * @property {string} title
  * @property {string} shortDescription
  * @property {ProjectTag[]} tags
- * @property {number} numMentors 
+ * @property {number} numMentors
  */
 /**
  * A list of card-based project previews.
  * @param {{projectPreviews: ProjectPreviewData}} props
- * @return {React.Component} Returns a React component which is a list of 
- * project previews.  
+ * @return {React.Component} Returns a React component which is a list of
+ * project previews.
  */
 export default function ProjectList(props) {
   return (
     <div className="p-4 row">
-      {props.projectPreviews.map((projectPreview, i) => 
+      {props.projectPreviews.map((projectPreview, i) =>
           <ProjectPreview key={i} projectPreview={projectPreview} />)}
     </div>
   )
