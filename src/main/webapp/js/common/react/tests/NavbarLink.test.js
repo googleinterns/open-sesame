18040f973c29ae3ab6edcccde7c792646d28dc69
Richie Goulazian <rgoulazian@gmail.com>
2020-06-27 22:03:16 -0400

    Add tests for NavbarLink

---
+++ src/main/webapp/js/common/react/tests/NavbarLink.test.js
@@ -0,0 +1,24 @@
+// Global to mimic the global variables available within a browser environment.
+// For example, React and ReactDOM, which are normally loaded from the script
+// tags in the HTML. 
+import React from 'react';
+import NavbarLink from '../components/NavbarLink.js';
+import '@testing-library/jest-dom/extend-expect';
+import {render, screen} from '@testing-library/react';
+
+describe('NavbarLink component', () => {
+  it('is rendered', () => {
+    const elem = render(<NavbarLink href="/home.html" name="Home" />);
+    expect(elem.container).not.toBeNull();
+  });
+
+  it('displays the name', () => {
+    const elem = render(<NavbarLink href="/home.html" name="Home" />);
+    expect(screen.queryByText('Home')).not.toBeNull();
+  });
+
+  it ('contains the intended href', () => {
+    const elem = render(<NavbarLink href="/home.html" name="Home" />);
+    expect(screen.queryByText('Home')).toHaveAttribute('href', '/home.html');
+  });
+});
