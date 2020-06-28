863c1416a39088e051c4ce71030a72e2f21cb09b
Richie Goulazian <rgoulazian@gmail.com>
2020-06-27 22:06:20 -0400

    Apply ESLint corrections

--- src/main/webapp/js/common/react/tests/NavbarLink.test.js
+++ src/main/webapp/js/common/react/tests/NavbarLink.test.js
@@ -1,6 +1,6 @@
 // Global to mimic the global variables available within a browser environment.
 // For example, React and ReactDOM, which are normally loaded from the script
-// tags in the HTML. 
+// tags in the HTML.
 import React from 'react';
 import NavbarLink from '../components/NavbarLink.js';
 import '@testing-library/jest-dom/extend-expect';
@@ -17,7 +17,7 @@ describe('NavbarLink component', () => {
     expect(screen.queryByText('Home')).not.toBeNull();
   });
 
-  it ('contains the intended href', () => {
+  it('contains the intended href', () => {
     const elem = render(<NavbarLink href="/home.html" name="Home" />);
     expect(screen.queryByText('Home')).toHaveAttribute('href', '/home.html');
   });
