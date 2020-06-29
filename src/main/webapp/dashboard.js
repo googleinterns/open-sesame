726da4520d7abb8057c8de8c9eab0a7af25e684c
Obi Abii <obiabii@google.com>
2020-06-29 14:46:17 -0400

    improve code readability

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -1,7 +1,23 @@
-/**
- * Div of the About me tab.
+// IDs
+/** 
+ * The ids of the various elements in my document
+ * @type {String}
  */
-const aboutMeDiv = document.getElementById('about-me');
+
+
+
+// ELEMENTS_FOR_ABOUT_ME_SECTION
+/**
+ * Elements from dashboard.html.
+ * @type {HTMLElement}
+ */
+const ABOUT_ME_CARD_DIV = document.getElementById('about-me-card-body');
+const ABOUT_ME_SECTION_DIV = document.getElementById('about-me');
+const USER_BIO = document.getElementById('user-bio');
+const USER_IMAGE = document.getElementById('user-image');
+const USER_NAME_AND_LOCATION = document.getElementById('user-name-location');
+const USER_TAG_ROW = document.getElementById('user-tag-row');
+const USER_GITHUB_BUTTON = document.getElementById('user-github')
 
 /**
  * Fake information for the hardcoded stage.
@@ -18,77 +34,76 @@ const dummyTags = ['Weekly', '30 mins', 'Kubernetes', 'Open Sesame',
 const dummyGitSrc = '#';
 
 /**
- * Creates a card element containing information about a given user.
+ * Populates the card element 'ABOUT_ME_CARD_DIV' with information about a
+ * given user.
  * @param {String} name
  * @param {String} location
  * @param {String} bio
  * @param {String} imgSrc
  * @param {String List} tags
- * @return {HTMLElement} About me Card
  */
-function createAboutMe(name, location, bio, imgSrc, tags, gitSrc) {
-  let AboutMeCard = document.createElement('div');
-  AboutMeCard.className = 'card card-holder col-12';
+function createAboutMe(name, location, bio, profileImgSrc, tags, gitSrc) {
+  let editButton = createEditButton('#');
+  ABOUT_ME_CARD_DIV.prepend(editButton);
 
-  let AboutMeCardBody = document.createElement('div');
-  AboutMeCardBody.className = 'card-body';
-  AboutMeCard.append(AboutMeCardBody);
+  USER_IMAGE.src = profileImgSrc;
 
+  USER_NAME_AND_LOCATION.innerHTML = name + '<br>';
+  let userLocation = createLocation(location);
+  USER_NAME_AND_LOCATION.append(userLocation);
+
+  USER_BIO.innerText = bio
+
+  USER_GITHUB_BUTTON.href = gitSrc;
+
+  for (tag of tags) {
+    addTag(tag, USER_TAG_ROW);
+  }
+}
+
+/**
+ * Creates an edit button element containing that links to the action 
+ * @param link.
+ * @param {String} link the href link to the action the edit button will perform
+ * @return {HTMLElement} edit button
+ */
+function createEditButton(link) {
   let editButton = document.createElement('a');
   editButton.className = 'float-right bold';
   editButton.innerText = 'Edit';
-  editButton.href = '#';
-  AboutMeCardBody.append(editButton);
-
-  let userImage = document.createElement('img');
-  userImage.className = 'card-img-top img-thumbnail w-25 p-2 m-2 mx-auto' +
-    ' d-block';
-  userImage.src = imgSrc;
-  AboutMeCardBody.append(userImage);
-
-  let userNameAndLocation = document.createElement('h4');
-  userNameAndLocation.id = 'user-name';
-  userNameAndLocation.className = 'card-title dark-emph';
-  userNameAndLocation.innerHTML = name + '<br>';
-  AboutMeCardBody.append(userNameAndLocation);
-
-  let userLocation = document.createElement('small');
-  userLocation.innerText = location;
-  userNameAndLocation.append(userLocation);
-
-  let userBio = document.createElement('p');
-  userBio.className = 'card-text';
-  userBio.innerText = bio;
-  AboutMeCardBody.append(userBio);
-
-  let gitButton = document.createElement('a');
-  gitButton.className = 'btn btn-primary';
-  gitButton.setAttribute('role', 'button');
-  gitButton.innerText = 'Github';
-  gitButton.href = gitSrc;
-  AboutMeCardBody.append(gitButton);
-
-  let tagRow = document.createElement('div');
-  tagRow.className = 'row p-3';
-  AboutMeCard.append(tagRow);
-  for (tag of tags) {
-    let tagElement = document.createElement('div');
-    tagElement.className = 'border border-muted text-muted mr-1 mb-1 badge';
-    tagElement.innerText = tag;
-    tagRow.append(tagElement);
-  }
-
-  return AboutMeCard;
+  editButton.href = link;
+  return editButton;
 }
 
+/**
+ * Adds a tag with @param tagText to @param tagDiv
+ * @param {String} tagText the text in the tag element.
+ * @param {HTMLElement} tagDiv the div the tag is to be added to.
+ */
+function addTag(tagText, tagDiv) {
+  let tagElement = document.createElement('div');
+  tagElement.className = 'border border-muted text-muted mr-1 mb-1 badge';
+  tagElement.innerText = tagText;
+  tagDiv.append(tagElement);
+}
 
+/**
+ * Creates a small element with a given @param location 
+ * @param {String} location text for a given location.
+ * @return {HTMLElement} location small element
+ */
+function createLocation(location) {
+  let userLocation = document.createElement('small');
+  userLocation.innerText = location;
+  return userLocation;
+}
 
 /**
  * Call functions to populate page sections with data.
  */
 function setUpPage() {
-  aboutMeDiv.append(createAboutMe(dummyName, dummyLocation, dummyBio,
-    dummyImg, dummyTags, dummyGitSrc));
+  createAboutMe(dummyName, dummyLocation, dummyBio, dummyImg, dummyTags,
+    dummyGitSrc);
 }
 
 window.onload = setUpPage;
