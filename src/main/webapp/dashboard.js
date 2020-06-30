49e193459ca8da831b59f487781be094b6a27184
Obi Abii <obiabii@google.com>
2020-06-30 11:56:31 -0400

    add project affiliation data

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -1,14 +1,18 @@
-// ELEMENTS_FOR_ABOUT_ME_SECTION
 /**
- * Elements from dashboard.html.
- * @type {HTMLElement}
+ * A project
+ * @typedef {Object} Project
+ * @property {String} name - the name of a given project.
+ * @property {Mentee List} mentees - the list of mentees under a given project.
  */
-const ABOUT_ME_CARD_DIV = document.getElementById('about-me-card-body');
-const USER_BIO = document.getElementById('user-bio');
-const USER_IMAGE = document.getElementById('user-image');
-const USER_NAME_AND_LOCATION = document.getElementById('user-name-location');
-const USER_TAG_ROW = document.getElementById('user-tag-row');
-const USER_GITHUB_BUTTON = document.getElementById('user-github');
+
+/**
+ * A mentee
+ * @typedef {Object} Mentee
+ * @property {String} name - the name of the mentee
+ * @property {String} starLink - the link to the action that rewards a given 
+ *                               mentee with a star
+ * @property {String} image -the link to 
+ */
 
 /**
  * Fake information for the hardcoded stage.
@@ -23,9 +27,29 @@ const dummyTags = ['Weekly', '30 mins', 'Kubernetes', 'Open Sesame',
   'Documentation',
 ];
 const dummyGitSrc = '#';
+const dummyProjectName = 'Kubernetes';
+const dummyMentees = [
+  { name: 'Richi', starLink: '#givestar', image: dummyImg },
+  { name: 'Obi', starLink: '#givestar', image: dummyImg },
+  { name: 'Sami', starLink: '#givestar', image: dummyImg },
+];
+const dummyProject = { name: 'Kubernetes', mentees: dummyMentees };
+
+// ELEMENTS_FOR_ABOUT_ME_SECTION
+/**
+ * Elements from dashboard.html.
+ * @type {HTMLElement}
+ */
+const ABOUT_ME_CARD_DIV = document.getElementById('about-me-card-body');
+const USER_BIO = document.getElementById('user-bio');
+const USER_IMAGE = document.getElementById('user-image');
+const USER_NAME_AND_LOCATION = document.getElementById('user-name-location');
+const USER_TAG_ROW = document.getElementById('user-tag-row');
+const USER_GITHUB_BUTTON = document.getElementById('user-github');
+const AFFILIATIONS_DIV = document.getElementById('affiliations');
 
 /**
- * Populates the card element 'ABOUT_ME_CARD_DIV' with information about a
+ * Populate the card element 'ABOUT_ME_CARD_DIV' with information about a
  * given user.
  * @param {String} name
  * @param {String} location
@@ -53,9 +77,9 @@ function createAboutMe(name, location, bio, profileImgSrc, tags, gitSrc) {
 }
 
 /**
- * Creates an edit button element that links to the action @param link.
+ * Return an edit button element that links to the action @param link.
  * @param {String} link the href link to the action the edit button will perform
- * @return {HTMLElement} edit button
+ * @returns {HTMLElement} edit button
  */
 function createEditButton(link) {
   let editButton = document.createElement('a');
@@ -66,7 +90,7 @@ function createEditButton(link) {
 }
 
 /**
- * Adds a tag with @param tagText to @param tagDiv
+ * Append a tag with @param tagText to @param tagDiv
  * @param {String} tagText the text in the tag element.
  * @param {HTMLElement} tagDiv the div the tag is to be added to.
  */
@@ -80,7 +104,7 @@ function addTag(tagText, tagDiv) {
 /**
  * Creates a small element with a given @param location
  * @param {String} location text for a given location.
- * @return {HTMLElement} location small element
+ * @returns {HTMLElement} location small element
  */
 function createLocation(location) {
   let userLocation = document.createElement('small');
@@ -89,11 +113,103 @@ function createLocation(location) {
 }
 
 /**
+ * Populate the AFFILIATION_DIV with information about the commitments a mentor
+ * has made to @param projectObject.
+ * @param {Project} projectObject
+ */
+function addProject(projectObject) {
+  let projectCardElement = createProjectCard(projectObject.name)
+  AFFILIATIONS_DIV.append(projectCardElement);
+  menteeRow = createRowElement();
+  for (mentee of projectObject.mentees) {
+    menteeRow.append(createMenteeCard(mentee));
+  }
+  projectCardElement.append(menteeRow);
+}
+
+/**
+ * Return a project card div element with the title @param projectName
+ * @param {String} projectName
+ * @returns {HTMLElement} div representing a project card with the 
+ * title @param projectName
+ */
+function createProjectCard(projectName) {
+  let projectCardElement = document.createElement('div');
+  projectCardElement.className = 'card container card-holder col-12' +
+    ' text-center project-card p-3 m-3';
+  let projectTitleElement = createCardTitle(projectName);
+  projectCardElement.append(projectTitleElement);
+  return projectCardElement;
+}
+
+/**
+ * Return a bootstrap card-title div element with @param titleText
+ * @param {String} titleText
+ * @returns {HTMLElement} div representing a card title with @param titleText 
+ */
+function createCardTitle(titleText) {
+  let cardTitleElement = document.createElement('h4');
+  cardTitleElement.className = 'card-title dark-emph';
+  cardTitleElement.innerText = titleText;
+  return cardTitleElement;
+}
+
+function createMenteeCard(mentee) {
+  let menteeCardElement = document.createElement('div');
+  menteeCardElement.className = 'card card-holder col-' +
+    ' text-center m-1 p-2';
+  let menteeCardBody = document.createElement('div');
+  menteeCardBody.className = 'card-body';
+  menteeCardElement.append(menteeCardBody);
+  menteeCardBody.append(createMenteeCardImage(mentee.image));
+  menteeCardBody.append(createCardTitle(mentee.name));
+  menteeCardBody.append(createStarButton(mentee.starLink));
+  return menteeCardElement
+}
+
+/**
+ * Return a bootstrap row element
+ * @returns {HTMLElement} bootstrap row div 
+ */
+function createRowElement() {
+  let rowElement = document.createElement('div');
+  rowElement.className = 'row p-3';
+  return rowElement;
+}
+
+/**
+ * Return an anchor of the class star that perform the acton @param link 
+ * @param {String} link
+ * @returns {HTMLElement} anchor that performs the given action
+ */
+function createStarButton(link) {
+  let starButton = document.createElement('a');
+  starButton.className = 'star'
+  starButton.innerText = 'Send star';
+  starButton.href = link;
+  return starButton;
+}
+
+/**
+ * Return a small image with the source @param imgSrc
+ * @param {String} imgSrc 
+ * @returns {HTMLElement} image object
+ */
+function createMenteeCardImage(imgSrc) {
+  let menteeCardImage = document.createElement('img');
+  menteeCardImage.className = 'card-img-top mentee-card-img';
+  menteeCardImage.src = imgSrc;
+  return menteeCardImage;
+}
+
+/**
  * Call functions to populate page sections with data.
  */
 function setUpPage() {
   createAboutMe(dummyName, dummyLocation, dummyBio, dummyImg, dummyTags,
     dummyGitSrc);
+  addProject(dummyProject);
+  addProject(dummyProject);
 }
 
 window.onload = setUpPage;
