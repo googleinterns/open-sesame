6f6c0bc1164a690c2c7a9b38476d7c163b690812
Richie Goulazian <rgoulazian@gmail.com>
2020-06-30 18:57:18 -0400

    Add tests

--- src/test/java/com/google/opensesame/projects/ProjectPreviewTest.java
+++ src/test/java/com/google/opensesame/projects/ProjectPreviewTest.java
@@ -1,51 +1,44 @@
 package com.google.opensesame.projects;
 
-import com.google.opensesame.github.GitHubGetter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.kohsuke.github.GitHub;
+import org.junit.Test;
+import org.kohsuke.github.GHRepository;
+import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class ProjectPreviewTest {
   private static Map<String, Object> projectProperties;
-  private static GitHub gitHub;
+  private static GHRepository mockRepository;
 
-  private static final String TEST_REPO_NAME = "octocat/Hello-World";
-  private static final int TEST_REPO_NUM_MENTORS = 5;
+  private static final String MOCK_REPO_NAME = "Hello-World";
+  private static final String MOCK_REPO_DESCRIPTION = "This is a test description.";
+  private static final int MOCK_REPO_NUM_MENTORS = 5;
 
-  private static int GITHUB_LIMIT_REMAINING;
-
-  @Before
-  public void setUp() throws IOException {
-    gitHub = GitHubGetter.getGitHub();
-    GITHUB_LIMIT_REMAINING = gitHub.rateLimit().getRemaining();
+  @BeforeClass
+  public static void setUp() throws IOException {
+    mockRepository = Mockito.mock(GHRepository.class);
+    Mockito.when(mockRepository.getName()).thenReturn(MOCK_REPO_NAME);
+    Mockito.when(mockRepository.getDescription()).thenReturn(MOCK_REPO_DESCRIPTION);
 
     projectProperties = new HashMap<String, Object>();
-    projectProperties.put("repositoryName", TEST_REPO_NAME);
-    projectProperties.put("numMentors", TEST_REPO_NUM_MENTORS);
-  }
-
-  @After
-  public void getUsedAPICalls() throws IOException {
-    int nowRemaining = gitHub.rateLimit().getRemaining();
-    int callsUsed = GITHUB_LIMIT_REMAINING - nowRemaining;
-    System.out.println(
-        "Test used " + callsUsed + " GitHub API calls; " + nowRemaining + " remaining.");
+    projectProperties.put("numMentors", MOCK_REPO_NUM_MENTORS);
   }
 
   @Test
   public void populatesRepositoryInfo() throws IOException {
-    // Ensures that the project preview data is populated from the GitHub repository.
-    ProjectPreview projectPreview = ProjectPreview.fromProperties(projectProperties);
+    // Ensures that the project preview data is properly populated from the GitHub repository and
+    // datastore entity properties.
+    ProjectPreview projectPreview = 
+        ProjectPreview.fromPropertiesAndRepository(projectProperties, mockRepository);
 
-    Assert.assertEquals("My first repository on GitHub!", projectPreview.getShortDescription());
-    Assert.assertEquals("Hello-World", projectPreview.getName());
+    Assert.assertEquals(MOCK_REPO_NAME, projectPreview.getName());
+    Assert.assertEquals(MOCK_REPO_DESCRIPTION, projectPreview.getShortDescription());
+    Assert.assertEquals(MOCK_REPO_NUM_MENTORS, projectPreview.getNumMentors());
   }
 }
