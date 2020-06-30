7594673759200015bc37424e87b8477a522c7b82
Richie Goulazian <rgoulazian@gmail.com>
2020-06-30 12:38:47 -0400

    Add project preview to backend

---
+++ src/main/java/com/google/opensesame/servlets/ProjectPreviewsServlet.java
@@ -0,0 +1,35 @@
+package com.google.opensesame.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.opensesame.projects.ProjectPreview;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+
+@WebServlet("/project-previews")
+public class ProjectPreviewsServlet extends HttpServlet {
+
+  private final Gson gson = new Gson();
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Add validation and database querying here.
+    Entity testEntity = new Entity("Project");
+    testEntity.setProperty("repositoryName", "tensorflow/tensorflow");
+    testEntity.setProperty("numMentors", 5);
+
+    ArrayList<ProjectPreview> projectPreviews = new ArrayList<ProjectPreview>();
+    projectPreviews.add(ProjectPreview.fromEntity(testEntity));
+
+    String projectPreviewsJson = gson.toJson(projectPreviews);
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.setContentType("application/json;");
+    response.getWriter().println(projectPreviewsJson);
+  }
+}
