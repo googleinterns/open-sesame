2a2133f2f1ae8a2af53b9ef5f26eecf6ba2450cf
Richie Goulazian <rgoulazian@gmail.com>
2020-07-06 16:57:08 -0400

    Add comments and improve style

--- src/main/webapp/js/projects/react/components/ProjectsSearch.js
+++ src/main/webapp/js/projects/react/components/ProjectsSearch.js
@@ -5,7 +5,15 @@
 import ProjectList from './ProjectList.js';
 import standardizeFetchErrors from '../../../fetch_handler.js';
 
+/**
+ * The main project search page react component.
+ * @extends React.Component 
+ */
 export default class ProjectsSearch extends React.Component {
+  /**
+   * Create the projects search page react component.
+   * @param {{}} props 
+   */
   constructor(props) {
     super(props);
 
@@ -14,13 +22,16 @@ export default class ProjectsSearch extends React.Component {
     };
   }
 
+  /**
+   * Loads the project preview data after React has initialized the component.
+   */
   componentDidMount() {
     const fetchRequest = standardizeFetchErrors(
         fetch('/project-previews'),
-        "Failed to communicate with the server, please try again later.",
-        "Encountered a server error, please try again later.");
+        'Failed to communicate with the server, please try again later.',
+        'Encountered a server error, please try again later.');
     fetchRequest.then((projectPreviews) => {
-      console.log("Project Previews Received:");
+      console.log('Project Previews Received:');
       console.log(projectPreviews);
       this.setState({
         isFetching: false,
@@ -33,9 +44,15 @@ export default class ProjectsSearch extends React.Component {
     });
   }
 
+  /**
+   * Renders the list of project previews or a loading notification if the data
+   * is still being loaded.
+   */
   render() {
     if (this.state.isFetching) {
-      return <h1 className="text-primary text-center mt-2">Loading projects...</h1>;
+      return (
+        <h1 className="text-primary text-center mt-2">Loading projects...</h1>
+      );
     }
 
     return <ProjectList projectPreviews={this.state.projectPreviews} />;
