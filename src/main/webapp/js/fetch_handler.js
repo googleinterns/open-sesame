db152b73969fc1aee70a1aadede5cd56fbf819f9
Richie Goulazian <rgoulazian@google.com>
2020-07-06 16:45:39 -0400

    Apply style changes from ESLint

--- src/main/webapp/js/fetch_handler.js
+++ src/main/webapp/js/fetch_handler.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * @fileoverview Formats the error returned from a fetch to the Open Sesame API.
  * There are three different types of errors that are formatted here: a network
  * error from the Fetch API, a server error from the Open Sesame API, and a
@@ -11,13 +11,13 @@
  * @property {string} userMessage
  */
 /**
- * Standardizes the errors of a fetch request to the Open Sesame backend 
- * for easier error handling. 
+ * Standardizes the errors of a fetch request to the Open Sesame backend
+ * for easier error handling.
  * {@link ErrorResponse}
- * @param {Promise} fetchRequest A fetch request to the Open Sesame backend. 
+ * @param {Promise} fetchRequest A fetch request to the Open Sesame backend.
  * @param {string} fetchFailedUserMessage The message to display to the user
  *    when the fetch itself has failed due to a network error.
- * @param {string} genericServerErrorUserMessage The generic message to display 
+ * @param {string} genericServerErrorUserMessage The generic message to display
  *    to the user when a server error has been encountered.
  * @return {Promise} Returns a promise that formats the errors from a fetch
  *    request.
@@ -25,7 +25,7 @@
 export default function standardizeFetchErrors(
     fetchRequest, fetchFailedUserMessage, genericServerErrorUserMessage) {
   return fetchRequest.catch((fetchError) => {
-    // Fetch encountered an error while making the request (most likely a 
+    // Fetch encountered an error while making the request (most likely a
     // network error).
     /** @type {ErrorResponse} */
     const errorResponse = {
@@ -49,13 +49,14 @@ export default function standardizeFetchErrors(
  * Formats an API error response to match the ErrorResponse convention.
  * {@link ErrorResponse}
  * @param {Response} errorResponse
+ * @param {string} genericServerErrorUserMessage
  * @return {Promise} Returns a promise that rejects with a properly-formatted
  *    ErrorResponse object. {@link ErrorResponse}
  */
 function formatAPIErrorResponse(errorResponse, genericServerErrorUserMessage) {
   if (errorResponse.headers.get('Content-Type').startsWith('text/html')) {
     // HTML-based error responses are received from the servlet-based errors
-    // of the Open Sesame API. For example, the servlet has encountered an 
+    // of the Open Sesame API. For example, the servlet has encountered an
     // internal error and could not continue with the request
     // or a 404 error if the servlet could not find a requested endpoint.
     return errorResponse.text().then((errorText) => {
@@ -65,17 +66,17 @@ function formatAPIErrorResponse(errorResponse, genericServerErrorUserMessage) {
         userMessage: genericServerErrorUserMessage,
       });
     });
-  } else if (errorResponse.headers.get("Content-Type")
+  } else if (errorResponse.headers.get('Content-Type')
       .startsWith('application/json')) {
-    // JSON-based error responses are received from the defined behavior 
-    // for the Open Sesame API. These responses follow the same format as 
+    // JSON-based error responses are received from the defined behavior
+    // for the Open Sesame API. These responses follow the same format as
     // the ErrorResponse type.
     return errorResponse.json().then((errorJson) => {
       return Promise.reject(errorJson);
     });
   } else {
     return Promise.reject({
-      error: "Error could not be parsed; unanticipated content type.",
+      error: 'Error could not be parsed; unanticipated content type.',
       statusCode: errorResponse.status,
       userMessage: genericServerErrorUserMessage,
     });
@@ -88,9 +89,9 @@ function formatAPIErrorResponse(errorResponse, genericServerErrorUserMessage) {
  * @return {string} Returns the error text.
  */
 function extractErrorTextFromHTML(htmlString) {
-  const responseDOM = new DOMParser().parseFromString(htmlString, "text/html");
-  const body = responseDOM.getElementsByTagName("BODY")[0];
+  const responseDOM = new DOMParser().parseFromString(htmlString, 'text/html');
+  const body = responseDOM.getElementsByTagName('BODY')[0];
 
   return Array.from(body.childNodes)
-      .map((child) => child.textContent).join(" ");
+      .map((child) => child.textContent).join(' ');
 }
