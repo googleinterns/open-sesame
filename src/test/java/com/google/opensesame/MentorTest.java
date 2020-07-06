9b818b07423af1e29e3d153cccc675c11e0407b2
Sami Alves <scalves00@gmail.com>
2020-07-06 21:12:09 +0000

    Simplify naming.

--- src/test/java/com/google/opensesame/MentorTest.java
+++ src/test/java/com/google/opensesame/MentorTest.java
@@ -8,22 +8,22 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class PersonTests {
-  private static final String MENTOR_1_NAME = "Mentor 1";
-  private static final String MENTOR_1_GTHUB = "Mentor_1_GitHub_ID";
-  private static final String PROJECT_1 = "Project 1";
+  private static final String MENTOR_NAME = "Mentor";
+  private static final String MENTOR_GITHUB_ID = "Mentor_GitHub_ID";
+  private static final String PROJECT_ID = "Project_ID";
 
   @Test
   public void addProjects() {
-    MentorObject mentor1 = new MentorBuilder()
-        .name(MENTOR_1_NAME)
-        .gitHubID(MENTOR_1_GITHUB)
+    MentorObject mentor = new MentorBuilder()
+        .name(MENTOR_NAME)
+        .gitHubID(MENTOR_GITHUB_ID)
         .buildMentor();
-    mentor1.addProject(PROJECT_1);
+    mentor.addProject(PROJECT_ID);
     
     List<String> expected = new List<String>;
-    expected.add(PROJECT_1);
+    expected.add(PROJECT_ID);
 
-    List<String> actual = mentor1.getProjectIDs();
+    List<String> actual = mentor.getProjectIDs();
 
     Assert.assertEquals(expected, actual);
   }
