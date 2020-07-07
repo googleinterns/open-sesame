3c428b23e2fe9476a95f22623a0ad8c068642ffe
Obi Abii <coa22@cornell.edu>
2020-07-07 14:51:28 -0400

    remove redundant comment
    Co-authored-by: Bill Yeh <billyeh@google.com>

--- src/main/webapp/authorization.js
+++ src/main/webapp/authorization.js
@@ -16,7 +16,6 @@ function initializeFirebase() {
  * Function called when clicking the Login/Logout button.
  */
 function toggleSignIn() {
-  // if not logged in
   if (!firebase.auth().currentUser) {
     let provider = new firebase.auth.GithubAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
