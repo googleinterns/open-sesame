90f6ba7a120360a9cb147c68bb76b8bbf10fe651
Obi Abii <obiabii@google.com>
2020-07-09 14:20:26 -0400

    remove wildcard import

--- src/test/java/com/google/opensesame/MentorTest.java
+++ src/test/java/com/google/opensesame/MentorTest.java
@@ -1,12 +1,13 @@
 package com.google.opensesame;
 
-import com.google.opensesame.servlets.*;
+import com.google.opensesame.servlets.MentorBuilder;
+import com.google.opensesame.servlets.MentorObject;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public class MentorTest {
