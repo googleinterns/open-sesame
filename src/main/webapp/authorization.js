e97e89d2a2dba7d3c8b9bc0bc729a8887c69a2ec
Obi Abii <obiabii@google.com>
2020-07-08 22:28:31 -0400

    improve file comments

--- src/main/webapp/authorization.js
+++ src/main/webapp/authorization.js
@@ -7,9 +7,15 @@ const firebaseConfig = {
 /**
  * Object used for Github authorization and as a wrapper for firebase.
  * It handles Firebase initialization on instantiation and holds nifty
- * functions for handling github authorization
+ * functions for handling github authorization. We are using a class to have
+ * access tokens persist for ease opf use on the backend
  * 
- * make sure to include this script when using
+ * Make sure to include the scripts below in any HTML File that uses this class
+ * 
+ * <script src="https://www.gstatic.com/firebasejs/7.15.5/firebase.js"></script>
+ * <script type="module" src="authorization.js"></script>
+ * 
+ * NOTE: The authorization.js file location mighjt change.
  */
 class GithubAuthorizer {
   constructor() {
@@ -22,15 +28,20 @@ class GithubAuthorizer {
    * The most recent access token for the Github API. To refresh this, the user
    * must be signed out and signed in again. It is important to keep in mind
    * that this token expires and must be used swiftly after calling
-   * toggleSignIn(). If sign is yet to be toggled, this will return an empty
-   * string
+   * toggleSignIn(). If authorization is yet to be toggled, this will return 
+   * null. If an error occurs during authorization, it will again be set to
+   * null. This property will be set to null on sign out as well.
+   * 
+   * NOTE: In the event that an access token times out, the token will still be
+   * available here and so, a non-null token is not a good sign of a valid
+   * token. 
    */
   get token() {
     this.token;
   }
 
   /**
-   * the current user if signed in and null if not. For all intents and
+   * the current user if authorized and null if not. For all intents and
    * purposes, this function will serve as a true/false value in an if
    * statement.
    */
@@ -64,7 +75,7 @@ class GithubAuthorizer {
    * Side Effects: when authorizing a user, it sets the current objects token
    * to the returned Github API accessToken if a token is returned.
    */
-  toggleSignIn() {
+  toggleAuthorization() {
     firebase = this.firebase;
     if (!firebase.auth().currentUser) {
       let provider = new firebase.auth.GithubAuthProvider();
@@ -72,8 +83,10 @@ class GithubAuthorizer {
         // This gives you a GitHub Access Token.
         // You can use it to access the GitHub API.
         this.token = result.credential.accessToken;
+        console.log(token);
       }).catch(function(error) {
-        // Handle Errors here.
+        this.token = null
+          // Handle Errors here.
         console.error(error);
         // TODO: Change this in future iterations
         alert(error.message);
@@ -81,7 +94,7 @@ class GithubAuthorizer {
     } else {
       // if logged in, sign out
       firebase.auth().signOut();
-
+      this.token = null;
     }
   }
 
@@ -90,7 +103,7 @@ class GithubAuthorizer {
    * purposes, this function will serve as a true/false value in an if
    * statement.
    */
-  isSignedIn() {
+  isAuthorized() {
     return this.firebase.auth().currentUser;
   }
 }
