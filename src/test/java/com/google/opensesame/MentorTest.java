523162ea054e8d7d2cc724ac5fb469b894577316
Obi Abii <obiabii@google.com>
2020-07-09 12:12:13 -0400

    Fix MentoTest error from relocation

--- src/test/java/com/google/opensesame/MentorTest.java
+++ src/test/java/com/google/opensesame/MentorTest.java
@@ -1,11 +1,12 @@
-package com.google.opensesame.servlets;
+package com.google.opensesame;
 
+import com.google.opensesame.servlets.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public class MentorTest {
