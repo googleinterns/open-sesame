011599b751166a1e819089cda843482a968531bb
Richie Goulazian <rgoulazian@gmail.com>
2020-07-10 15:17:56 -0400

    Make auth button support profile creation

--- src/main/webapp/js/common/react/components/AuthButton.js
+++ src/main/webapp/js/common/react/components/AuthButton.js
@@ -1,6 +1,11 @@
 import checkTesting from '../../../checkTesting.js';
-import standardizeFetchErrors from '../../../fetch_handler.js';
+import {
+  standardizeFetchErrors, 
+  makeRelativeUrlAbsolute
+} from '../../../fetch_handler.js';
 checkTesting();
+
+const USER_SIGNUP_URL = '/signup.html';
 
 /**
  * Login/Logout button that fetches user authorization data to automatically
@@ -25,7 +30,8 @@ export default class AuthButton extends React.Component {
    * @override
    */
   componentDidMount() {
-    const fetchRequest = standardizeFetchErrors(fetch('/auth'),
+    const fetchRequest = standardizeFetchErrors(
+        fetch(makeRelativeUrlAbsolute('/auth')),
         'Failed to communicate with the server, please try again later.',
         'Encountered a server error, please try again later.');
 
@@ -49,20 +55,25 @@ export default class AuthButton extends React.Component {
    * @override
    */
   render() {
-    let classList = 'btn btn-primary';
+    let classList = 'btn btn-purple';
+    let linkText;
+    let authLink;
     if (this.state.isFetching) {
       // The button is disabled while the authorization data is being loaded.
       classList += ' disabled';
-    }
-
-    let linkText = 'Log In';
-    let authLink = '/';
-    console.log(this.state);
-    if (!this.state.isFetching) {
+      linkText = 'Log In';
+      authLink = '/';  
+    } else {
       if (this.state.authData.authorized) {
-        authLink = this.state.authData.logoutUrl;
-        linkText = 'Log Out';
+        if (this.state.authData.hasProfile) {
+          authLink = this.state.authData.logoutUrl;
+          linkText = 'Log Out'; 
+        } else {
+          authLink = USER_SIGNUP_URL;
+          linkText = 'Create Profile';
+        }
       } else {
+        linkText = 'Log In';
         authLink = this.state.authData.loginUrl;
       }
     }
