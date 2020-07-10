1647dc6b2b4c3737ef9c67ce06e91b389dce8c6e
Richie Goulazian <rgoulazian@gmail.com>
2020-07-10 15:13:13 -0400

    Remove auth button

--- src/main/webapp/js/common/react/components/Navbar.js
+++ src/main/webapp/js/common/react/components/Navbar.js
@@ -1,6 +1,5 @@
 import checkTesting from '../../../checkTesting.js';
 import NavbarLink from './NavbarLink.js';
-import AuthButton from './AuthButton.js';
 checkTesting();
 
 /**
@@ -31,9 +30,6 @@ export default function Navbar(props) {
           {props.urls.map((url, i) =>
               <NavbarLink key={i} href={url.href} name={url.name} />)}
         </ul>
-      </div>
-      <div className="ml-1">
-        <AuthButton />
       </div>
     </nav>
   );
