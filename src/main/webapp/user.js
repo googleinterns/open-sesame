60ce2bf37488e85e6ee5d606a70ddcf63d6a0c38
Obi Abii <obiabii@google.com>
2020-07-14 14:40:32 -0400

    Add error handling to the user.js post and get functions

--- src/main/webapp/user.js
+++ src/main/webapp/user.js
@@ -14,15 +14,37 @@ function getUser(user) { //eslint-disable-line
   const fetchRequest = fetch(makeRelativeUrlAbsolute('/user?githubID=' + user));
 
   const errorFormattedFetchRequest = standardizeFetchErrors(
-      fetchRequest,
-      'Failed to communicate with the server. Please try again later.',
-      'An error occcured while retrieving this account.' +
+    fetchRequest,
+    'Failed to communicate with the server. Please try again later.',
+    'An error occcured while retrieving this account.' +
     ' Please try again later.');
 
   return errorFormattedFetchRequest.then((response) => response.json())
-      .then((user) => {
-        return user;
-      });
+    .then((user) => {
+      return user;
+    })
 }
 
-export {getUser};
+/**
+ * Posts a user to the servlet
+ * @param {URLSearchParams} userParams parameters with the users information
+ */
+function postUser(userParams) {
+  const fetchRequest = fetch(makeRelativeUrlAbsolute('/user'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: userParams,
+  });
+
+  const errorFormattedFetchRequest = standardizeFetchErrors(
+    fetchRequest,
+    'Failed to communicate with the server. Please try again later.',
+    'An error occcured while creating your account.' +
+    ' Please try again later.');
+
+  return errorFormattedFetchRequest;
+}
+
+export { getUser, postUser };
