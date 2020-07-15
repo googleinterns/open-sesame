a2ab83ed8458ad39c54c74be23adcdfa34c369f7
Sami Alves <scalves00@gmail.com>
2020-07-15 17:48:38 +0000

    Resolve Merge Conflicts.

--- src/main/java/com/google/opensesame/servlets/PersonObject.java
+++ src/main/java/com/google/opensesame/servlets/PersonObject.java
@@ -1,43 +1,81 @@
 package com.google.opensesame.servlets;
 
+import com.google.opensesame.github.GitHubGetter;
+import java.io.IOException;
 import java.util.ArrayList;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
 
+/** A class containing basic user data. This is intended to be serialized and sent to the client. */
 public class PersonObject {
-  public static String ENTITY_NAME = "User";
-  public static String GITHUB_ID_FIELD = "github-id";
-  public static String TAG_LIST_FIELD = "tags";
+  private final ArrayList<String> interestTags;
+  private final String bio;
+  private final String email;
+  private final String gitHubID;
+  private final String gitHubURL;
+  private final String image;
+  private final String location;
+  private final String name;
 
-  private String name;
-  private String description;
-  private ArrayList<String> interestTags;
-  private String gitHubID;
-  private String email;
-
-  public PersonObject(
-      String name, String gitHubID, String description, ArrayList<String> interestTags) {
-    this.name = name;
+  /**
+   * Creates a PersonObject with the given parameters
+   *
+   * @param gitHubID
+   * @param interestTags
+   * @throws IOException
+   */
+  public PersonObject(String gitHubID, ArrayList<String> interestTags) throws IOException {
     this.gitHubID = gitHubID;
-    this.description = description;
     this.interestTags = interestTags;
+
+    // GitHub query TODO: alternate ways of getting this information.
+    final GitHub gitHub = GitHubGetter.getGitHub();
+    final GHUser userGitAccount = gitHub.getUser(gitHubID);
+    this.bio = userGitAccount.getBio();
+    this.email = userGitAccount.getEmail();
+    this.image = userGitAccount.getAvatarUrl();
+    this.location = userGitAccount.getLocation();
+    this.name = userGitAccount.getName();
+    this.gitHubURL = userGitAccount.getHtmlUrl().toString();
   }
 
+  /** @return The name of a user. */
   public String getName() {
-    return name;
+    return this.name;
   }
 
-  public String getEmail() {
-    return email;
+  /** @return The bio of a user from GitHub. */
+  public String getBio() {
+    return this.bio;
   }
 
-  public String getDescription() {
-    return description;
-  }
-
+  /** @return The tags associated with a user. */
   public ArrayList<String> getTags() {
-    return interestTags;
+    return this.interestTags;
   }
 
+  /** @return the GitHub ID of a user. */
   public String getGitHubID() {
-    return gitHubID;
+    return this.gitHubID;
+  }
+
+  /** @return the image associated with a user. */
+  public String getImage() {
+    return this.image;
+  }
+
+  /** @return the location associated with a user according to GitHub. */
+  public String getLocation() {
+    return this.location;
+  }
+
+  /** @return the email associated with a user according to GitHub. */
+  public String getEmail() {
+    return this.email;
+  }
+
+  /** @return the URL of the current User page on GitHub. */
+  public String getGitHubURL() {
+    return this.gitHubURL;
   }
 }
