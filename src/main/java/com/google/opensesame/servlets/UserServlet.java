64bd9168033bc6e6d3a374fc4cd462be3580f71a
Obi Abii <obiabii@google.com>
2020-07-14 20:24:34 -0400

    pivot from using githubID to using UserID as a datastore key name

--- src/main/java/com/google/opensesame/servlets/UserServlet.java
+++ src/main/java/com/google/opensesame/servlets/UserServlet.java
@@ -12,7 +12,12 @@ import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.gson.Gson;
+import com.google.opensesame.auth.AuthServlet;
 import com.google.opensesame.util.ErrorResponse;
+
+import org.kohsuke.github.GHMyself;
+import org.kohsuke.github.GitHub;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,10 +38,24 @@ public class UserServlet extends HttpServlet {
   }
 
   @Override
-  // Get a specific user. Return null if not found.
+  // Get a specific user. Return the currently signed in user if none is found.
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String userGithub = request.getParameter("githubID"); // TODO:Check for null values
-    Key userKey = KeyFactory.createKey(PersonBuilder.ENTITY_NAME, userGithub);
+
+    String  userID = request.getParameter("userID") != null ? 
+          request.getParameter("userID") : 
+          AuthServlet.getAuthorizedUser().getUserId();
+  
+          Key userKey; 
+    try {
+      userKey = KeyFactory.createKey(PersonBuilder.ENTITY_NAME, userID);
+    } catch (Exception e) {
+      ErrorResponse.sendJsonError(
+          response,
+          e.getMessage() + "/n/n" + e.getStackTrace(),
+          HttpServletResponse.SC_BAD_REQUEST,
+          "An error occured while getting the User's profile");
+      return;
+    }
 
     Entity userEntity; // TODO: Abstract doGet() into it's own function
     try {
@@ -73,22 +92,57 @@ public class UserServlet extends HttpServlet {
   @Override
   // Send a user to datastore. Update the current information about the user if one exists.
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String userGitHubID = request.getParameter("githubID");
-    if (userGitHubID == null || userGitHubID.trim().length() == 0) {
+    String userGitHubAuthToken;
+    ArrayList<String> userTags;
+    // Get information from request body
+    try{
+    userGitHubAuthToken = request.getParameter("gitHubAuthToken");
+    userTags =
+          new ArrayList<>(Arrays.asList(request.getParameterValues("interestTags")));
+    } 
+    catch (Exception e) {
       ErrorResponse.sendJsonError(
           response,
-          "Empty GitHub ID",
+          e.getMessage() + "/n/n" + e.getStackTrace() + "/n/n Incomplete POST body parameters",
           HttpServletResponse.SC_BAD_REQUEST,
-          "There is no GitHub ID affiliated with this request");
+          "User information was incomplete");
       return;
     }
-    String[] userTags = request.getParameterValues("tags");
-    ArrayList<String> userTagsList =
-        userTags == null ? new ArrayList<String>() : new ArrayList<>(Arrays.asList(userTags));
-
-    Entity personEntity =
-        new PersonBuilder().gitHubID(userGitHubID).interestTags(userTagsList).buildPersonEntity();
-    datastore.put(personEntity);
+    // Get User information from GitHub using the Oath token.
+      GHMyself userGHMyself;
+      try {
+      GitHub userPersonalGitHubInstance = GitHub.connectUsingOAuth(userGitHubAuthToken);
+      userGHMyself = userPersonalGitHubInstance.getMyself();
+      } catch (Exception e) {
+        ErrorResponse.sendJsonError(
+            response,
+            e.getMessage()
+            + "/n/n"
+            + e.getStackTrace()
+            + "/n/n Could not get Authenticate User with the given token",
+            HttpServletResponse.SC_BAD_REQUEST,
+            "User could not be authenticated by GitHub, please try again");
+        return;
+      }
+      // Build and send the User's datastore entity
+      try {
+      Entity personEntity =
+          new PersonBuilder()
+          .userID(AuthServlet.getAuthorizedUser().getUserId())
+          .gitHubID(userGHMyself.getLogin())
+          .email(userGHMyself.getEmail())
+          .interestTags(userTags)
+          .buildPersonEntity();
+      datastore.put(personEntity);
+    } catch (Exception e) {
+      ErrorResponse.sendJsonError(
+          response,
+          e.getMessage() + "/n/n" + e.getStackTrace() + 
+          "/n/n User informatiuon could not be sent to datastore",
+          HttpServletResponse.SC_BAD_REQUEST,
+          "User information could not be sent to datastore");
+      return;
+    }
   }
 
   // TODO: use function in other servlets.
