1ba7d29df53d095f42d022adcebd7adec9a278c7
Richie Goulazian <rgoulazian@gmail.com>
2020-07-15 22:02:21 -0400

    Make project previews clickable

--- src/main/webapp/js/projects/react/components/ProjectPreview.js
+++ src/main/webapp/js/projects/react/components/ProjectPreview.js
@@ -1,6 +1,9 @@
 import ProjectTagList from './ProjectTagList.js';
 import {projectPreviewType} from '../prop_types.js';
 
+const Router = ReactRouterDOM.BrowserRouter;
+const Link = ReactRouterDOM.Link;
+
 /**
  * A card-based preview of a project.
  * @param {{projectPreview: ProjectPreviewData}} props
@@ -15,24 +18,30 @@ export default function ProjectPreview(props) {
   projectTags.unshift(projectPreview.primaryLanguage);
 
   return (
-    <div className="p-1 col-lg-4">
-      <div className="project-preview card h-100">
-        <div className="card-body pb-0">
-          <h5 className="card-title text-primary">{projectPreview.name}</h5>
-          <ProjectTagList tags={projectTags} />
-          <p>{projectPreview.description}</p>
-        </div>
-        <div className=
-          "px-3 pb-3 mt-auto d-flex flex-wrap justify-content-center">
-          <span>
-            <strong className="h5 text-success mr-2">
-              {projectPreview.numMentors}
-            </strong>
-            Mentors
-          </span>
+    <Router basename="/projects.html">
+      <div className="p-1 col-lg-4">
+        <div className="project-preview card h-100">
+          <div className="card-body pb-0">
+            <Link 
+                to={"/" + projectPreview.repositoryId} 
+                className="stretched-link">
+              <h5 className="card-title text-primary">{projectPreview.name}</h5>
+            </Link>
+            <ProjectTagList tags={projectTags} />
+            <p>{projectPreview.description}</p>
+          </div>
+          <div className=
+            "px-3 pb-3 mt-auto d-flex flex-wrap justify-content-center">
+            <span>
+              <strong className="h5 text-success mr-2">
+                {projectPreview.numMentors}
+              </strong>
+              Mentors
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
