cfe92aaa4518fbd7bb042b06cb1aa8fe229d3072
Richie Goulazian <rgoulazian@gmail.com>
2020-07-15 21:06:56 -0400

    Add generalized data fetcher

---
+++ src/main/webapp/js/common/react/components/DataFetcher.js
@@ -0,0 +1,62 @@
+import checkTesting from '../../../checkTesting.js';
+import { basicErrorHandling } from '../../../fetch_handler';
+checkTesting();
+
+/**
+ * A generic data fetching component that can be used for fetching data for any
+ * component.
+ */
+export default class DataFetcher extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFetching: true,
+      data: null,
+    }
+
+    /**
+     * Controller used to abort fetch requests if the React component is
+     * unmounted during a request:
+     * https://stackoverflow.com/questions/31061838/how-do-i-cancel-an-http-fetch-request
+     */
+    this.abortController = new AbortController();
+  }
+
+  /**
+   * Called before the component is destroyed.
+   * @override
+   */
+  componentWillUnmount() {
+    // Abort ongoing fetch requests if there are any.
+    this.abortController.abort();
+  }
+
+  /**
+   * Called after the component is first rendered by React.
+   * @override
+   */
+  componentDidMount() {
+    // TODO : Add automatic retry functionality here.
+    // TOOD : Add better user notification system here.
+    this.props.getFetchRequest(this.abortController.signal).then((data) => {
+      console.log('Data received:');
+      console.log(data);
+      
+      this.setState({
+        isFetching: false,
+        data,
+      });
+    }).catch((error) => basicErrorHandling(error));
+  }
+  
+  render() {
+    // https://reactjs.org/docs/render-props.html
+    return this.props.render(this.state);
+  }
+}
+
+DataFetcher.propTypes = {
+  getFetchRequest: PropTypes.func.isRequired,
+  render: PropTypes.func.isRequired,
+};
