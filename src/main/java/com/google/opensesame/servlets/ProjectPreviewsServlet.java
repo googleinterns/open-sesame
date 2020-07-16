e645b84de8afd6ce29d3b00e39cba2398a99935d
Richie Goulazian <rgoulazian@gmail.com>
2020-07-16 18:42:41 -0400

    Change preview servlet query

--- src/main/java/com/google/opensesame/servlets/ProjectPreviewsServlet.java
+++ src/main/java/com/google/opensesame/servlets/ProjectPreviewsServlet.java
@@ -1,13 +1,15 @@
 package com.google.opensesame.servlets;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
-import com.google.opensesame.projects.ProjectDatastore;
 import com.google.opensesame.projects.ProjectEntity;
 import com.google.opensesame.projects.ProjectPreviewData;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,11 +22,10 @@ public class ProjectPreviewsServlet extends HttpServlet {
   private final Gson gson = new Gson();
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query projectsQuery =
-        new Query("Project").addSort(ProjectEntity.NUM_MENTORS_KEY, SortDirection.DESCENDING);
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {    
+    List<ProjectEntity> projectEntities =
+        ofy().load().type(ProjectEntity.class).order("-numMentors").list();
 
-    List<ProjectEntity> projectEntities = ProjectDatastore.getByQuery(projectsQuery);
     List<ProjectPreviewData> projectPreviews = new ArrayList<ProjectPreviewData>();
     for (ProjectEntity projectEntity : projectEntities) {
       projectPreviews.add(ProjectPreviewData.fromProjectEntity(projectEntity));
