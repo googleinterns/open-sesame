a0ad12073a0effe1abe4d4c09f4271e11cb250ec
Obinnabii <Obinnabii@users.noreply.github.com>
2020-07-16 14:21:25 +0000

    Apply ESLint changes.

--- src/main/webapp/dashboard.js
+++ src/main/webapp/dashboard.js
@@ -1,4 +1,4 @@
-import { getUser } from './user.js';
+import {getUser} from './user.js';
 
 /**
  * A project
@@ -38,20 +38,20 @@ const dummyProjectName = 'Kubernetes';
 
 /** @const {Mentee List} */
 const dummyMentees = [{
-    name: 'Richi',
-    starLink: '#givestar',
-    image: dummyImg,
-  },
-  {
-    name: 'Obi',
-    starLink: '#givestar',
-    image: dummyImg,
-  },
-  {
-    name: 'Sami',
-    starLink: '#givestar',
-    image: dummyImg,
-  },
+  name: 'Richi',
+  starLink: '#givestar',
+  image: dummyImg,
+},
+{
+  name: 'Obi',
+  starLink: '#givestar',
+  image: dummyImg,
+},
+{
+  name: 'Sami',
+  starLink: '#givestar',
+  image: dummyImg,
+},
 ];
 
 /** @const {Project} */
--- src/main/webapp/user.js
+++ src/main/webapp/user.js
@@ -14,15 +14,15 @@ function getUser(userID = null) { //eslint-disable-line
   const fetchRequest = fetch(makeRelativeUrlAbsolute(fetchURL));
 
   const errorFormattedFetchRequest = standardizeFetchErrors(
-    fetchRequest,
-    'Failed to communicate with the server. Please try again later.',
-    'An error occcured while retrieving this account.' +
+      fetchRequest,
+      'Failed to communicate with the server. Please try again later.',
+      'An error occcured while retrieving this account.' +
     ' Please try again later.');
 
   return errorFormattedFetchRequest.then((response) => response.json())
-    .then((user) => {
-      return user;
-    });
+      .then((user) => {
+        return user;
+      });
 }
 
 /**
@@ -41,12 +41,12 @@ function postUser(userParams) {
   });
 
   const errorFormattedFetchRequest = standardizeFetchErrors(
-    fetchRequest,
-    'Failed to communicate with the server. Please try again later.',
-    'An error occcured while creating your account.' +
+      fetchRequest,
+      'Failed to communicate with the server. Please try again later.',
+      'An error occcured while creating your account.' +
     ' Please try again later.');
 
   return errorFormattedFetchRequest;
 }
 
-export { getUser, postUser };
+export {getUser, postUser};
