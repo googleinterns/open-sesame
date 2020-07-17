56548c8b44bffd8a4f453e7a1c662e5f9bb8a813
Richie Goulazian <rgoulazian@gmail.com>
2020-07-17 19:37:52 -0400

    Add testing

--- src/main/webapp/js/common/react/tests/Navbar.test.js
+++ src/main/webapp/js/common/react/tests/Navbar.test.js
@@ -14,26 +14,74 @@ const navbarUrls = [
     name: 'Projects',
     requiresAuth: false,
   },
+  {
+    href: '/dashboard.html',
+    name: 'Dashboard',
+    requiresAuth: true,
+  }
 ];
+
+const mockAuthData = {
+  authorized: true,
+  hasProfile: true,
+};
 
 describe('Navbar component', () => {
   it('is added to the DOM', () => {
-    const elem = render(<Navbar urls={navbarUrls} loading={true} />);
+    const elem = render(
+        <Navbar urls={navbarUrls} loading={true} />);
     expect(elem.container.firstChild).not.toBeNull();
   });
 
   it('displays the names of the urls', () => {
-    render(<Navbar urls={navbarUrls} loading={true} />);
+    render(
+        <Navbar urls={navbarUrls} loading={false} authData={mockAuthData} />);
 
     expect(screen.queryByText('Home')).not.toBeNull();
     expect(screen.queryByText('Projects')).not.toBeNull();
+    expect(screen.queryByText('Dashboard')).not.toBeNull();
   });
 
-  it('contains both urls', () => {
-    render(<Navbar urls={navbarUrls} loading={true} />);
+  it('contains the urls', () => {
+    render(
+        <Navbar urls={navbarUrls} loading={false} authData={mockAuthData} />);
 
     expect(screen.getByText('Home')).toHaveAttribute('href', '/');
     expect(screen.getByText('Projects'))
         .toHaveAttribute('href', '/projects.html');
+    expect(screen.getByText('Dashboard'))
+        .toHaveAttribute('href', '/dashboard.html');
+  });
+
+  it('does not display a restricted link to an unauthenticated user', () => {
+    const authData = {
+      authorized: false,
+      hasProfile: false,
+    };
+
+    render(
+      <Navbar urls={navbarUrls} loading={false} authData={authData} />);
+
+    expect(screen.queryByText('Dashboard')).toBeNull();
+  });
+
+  it('does not display a restricted link to a user without a profile', () => {
+    const authData = {
+      authorized: true,
+      hasProfile: false,
+    };
+
+    render(
+      <Navbar urls={navbarUrls} loading={false} authData={authData} />);
+
+    expect(screen.queryByText('Dashboard')).toBeNull();
+  });
+
+  it('does not display the restricted link while loading auth data', () => {
+    render(<Navbar urls={navbarUrls} loading={true} />);
+
+    expect(screen.queryByText('Home')).not.toBeNull();
+    expect(screen.queryByText('Projects')).not.toBeNull();
+    expect(screen.queryByText('Dashboard')).toBeNull();
   });
 });
