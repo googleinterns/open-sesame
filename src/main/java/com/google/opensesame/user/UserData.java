32edb8ee0412cde90a1f83569ed1f66f8225e5c3
Obi Abii <obiabii@google.com>
2020-07-17 13:08:20 -0400

    Create User Entity and Data classes

---
+++ src/main/java/com/google/opensesame/user/UserData.java
@@ -0,0 +1,135 @@
+package com.google.opensesame.user;
+
+import com.google.opensesame.auth.AuthServlet;
+import com.google.opensesame.github.GitHubGetter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+
+/**
+ * A fully fleshed out User of OpenSesame containing information sourced from GitHub. To conserve
+ * API calls, It would be best to only build UserData where neccesary. The main use of this class is
+ * to send data to the frontend as a JSON with Gson.
+ */
+public class UserData {
+  private final ArrayList<String> interestTags;
+  private final ArrayList<String> menteeIDs;
+  private final ArrayList<String> projectIDs;
+  private final Boolean isMentor;
+  private final String bio;
+  private final String gitHubID;
+  private final String gitHubURL;
+  private final String image;
+  private final String location;
+  private final String name;
+  private final String userID;
+  private String email;
+
+  /**
+   * Create a UserData from a given UserEntity 
+   * 
+   * Note: Emails will only ever be stored in a UserObject if the current user is
+   * authorized with the AuthServlet.
+   * @param userEntity
+   * @throws IOException
+   */
+  public UserData(UserEntity userEntity)
+      throws IOException {
+    this.gitHubID = userEntity.gitHubId;
+    this.interestTags = userEntity.interestTags;
+    this.userID = userEntity.userId;
+    this.email = userEntity.email;
+    // Only ever send email stuff when a user is authorized
+    if (AuthServlet.getAuthorizedUser() != null) {
+      this.email = null;
+      }
+    this.projectIDs = userEntity.projectIDs;
+    this.menteeIDs = userEntity.menteeIDs;
+    this.isMentor = userEntity.isMentor;
+
+    // GitHub query TODO: alternate ways of getting this information.
+    final GitHub gitHub = GitHubGetter.getGitHub();
+    final GHUser userGitAccount = gitHub.getUser(gitHubID);
+    this.bio = userGitAccount.getBio();
+    this.image = userGitAccount.getAvatarUrl();
+    this.location = userGitAccount.getLocation();
+    this.name = userGitAccount.getName();
+    this.gitHubURL = userGitAccount.getHtmlUrl().toString();
+  }
+  /** @return true if user is a mentor for a project, false otherwise. */
+  public Boolean isMentor() {
+    return this.isMentor;
+  }
+
+  /** @return The name of a user. */
+  public String getName() {
+    return this.name;
+  }
+
+  /** @return The bio of a user from GitHub. */
+  public String getBio() {
+    return this.bio;
+  }
+
+  /** @return The tags associated with a user. */
+  public ArrayList<String> getTags() {
+    return this.interestTags;
+  }
+
+  /** @return the GitHub ID of a user. */
+  public String getGitHubID() {
+    return this.gitHubID;
+  }
+
+  /** @return the image associated with a user. */
+  public String getImage() {
+    return this.image;
+  }
+
+  /** @return the location associated with a user according to GitHub. */
+  public String getLocation() {
+    return this.location;
+  }
+
+  /** @return the email associated with a user according to GitHub. */
+  public String getEmail() {
+    return this.email;
+  }
+
+  /** @return the URL of the current User page on GitHub. */
+  public String getGitHubURL() {
+    return this.gitHubURL;
+  }
+
+  /** @return the URL of the current User page on GitHub. */
+  public String getUserID() {
+    return this.userID;
+  }
+
+  /** @return a list of ids of projects a user is involved in. */
+  public ArrayList<String> getProjectIDs() {
+    return this.projectIDs;
+  }
+
+  /** @return a list of ids of mentees a user is currently mentoring. */
+  public ArrayList<String> getMenteeIDs() {
+    return this.menteeIDs;
+  }
+
+    /**
+   * Add a project to the list of projects associated with this instance of the mentor object.
+   * Remember to store this change in datastore with a UserEntity.
+   */
+  public void addProject(String projectID) {
+    projectIDs.add(projectID);
+  }
+
+  /**
+   * Add a project to the list of projects associated with this instance of the mentor object.
+   * Remember to store this change in datastore with a UserEntity.
+   */
+  public void addMentee(String menteeID) {
+    projectIDs.add(menteeID);
+  }
+}
---
+++ src/main/java/com/google/opensesame/user/UserEntity.java
@@ -0,0 +1,72 @@
+package com.google.opensesame.user;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.OnSave;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import java.util.ArrayList;
+
+/**
+ * The User Entity is designed to directly interface with datastore using Objectify. It holds the
+ * neccesary information to generate a UserObject.
+ */
+@Entity
+public class UserEntity {
+  @Id String userId;
+  @Index Boolean isMentor;
+  String email;
+  String gitHubId;
+  /** The interests a user indicated during signup */
+  ArrayList<String> interestTags;
+   /** Datastore IDs of projects the given user has decided to mentor for. */
+   ArrayList<String> projectIDs = new ArrayList<String>();
+   /** Datastore IDs of mentees that have expressed interest in being mentored by this user. */
+   ArrayList<String> menteeIDs = new ArrayList<String>();
+
+  public UserEntity() {}
+
+  public UserEntity(UserData user) {
+    this.gitHubId = user.getGitHubID();
+    this.interestTags = user.getTags();
+    this.email = user.getEmail();
+    this.userId = user.getUserID();
+    this.projectIDs = user.getProjectIDs();
+    this.menteeIDs = user.getMenteeIDs();
+  }
+
+  public UserEntity(String userId, String gitHubId, ArrayList<String> interestTags, String email) {
+    this.userId = userId;
+    this.gitHubId = gitHubId;
+    this.interestTags = interestTags;
+    this.email = email;
+  }
+
+  public UserEntity(String userId, String gitHubId, ArrayList<String> interestTags,
+  String email, ArrayList<String> projectIDs, ArrayList<String> menteeIDs) {
+    this(userId, gitHubId, interestTags, email);
+    this.projectIDs = projectIDs;
+    this.menteeIDs = menteeIDs;
+  }
+
+  @OnSave
+  public void setIsMentor() {
+    this.isMentor = projectIDs.isEmpty();
+  }
+
+  /**
+   * Add a project Id to the list of project Ids associated with this instance of the mentor object.
+   * Remember to store this change in datastore with a personEntity.
+   */
+  public void addProject(String projectID) {
+    projectIDs.add(projectID);
+  }
+
+  /**
+   * Add a mentee Id to the list of mentee Ids associated with this instance of the mentor object.
+   * Remember to store this change in datastore with a personEntity.
+   */
+  public void addMentee(String menteeID) {
+    projectIDs.add(menteeID);
+  }
+}
