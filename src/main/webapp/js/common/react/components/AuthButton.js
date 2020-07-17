06b00a61dbe6d761a1346baa6e2bf2c329f20744
Richie Goulazian <rgoulazian@gmail.com>
2020-07-17 13:48:26 -0400

    Pull auth data fetch from authbutton

--- src/main/webapp/js/common/react/components/AuthButton.js
+++ src/main/webapp/js/common/react/components/AuthButton.js
@@ -1,84 +1,49 @@
 import checkTesting from '../../../checkTesting.js';
-import {
-  standardizeFetchErrors,
-  basicErrorHandling,
-  makeRelativeUrlAbsolute,
-} from '../../../fetch_handler.js';
+import {authDataType} from '../navbar_prop_types.js';
 checkTesting();
 
 const USER_SIGNUP_URL = '/signup.html';
 
 /**
- * Login/Logout button that fetches user authorization data to automatically
- * update its appearance.
- * @extends React.Component
+ * Login/Logout button that changes its appearance based on the inputted
+ * authentication data.
+ * @param {Object} props
+ * @return {React.Component} Returns the React component.
  */
-export default class AuthButton extends React.Component {
-  /**
-   * Create the AuthButton react component.
-   * @param {{}} props
-   */
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isFetching: true,
-    };
-  }
-
-  /**
-   * Loads user authorization data after React has initialized the component.
-   * @override
-   */
-  componentDidMount() {
-    const fetchRequest = standardizeFetchErrors(
-        fetch(makeRelativeUrlAbsolute('/auth')),
-        'Failed to communicate with the server, please try again later.',
-        'Encountered a server error, please try again later.');
-    fetchRequest.then((response) => response.json()).then((authData) => {
-      console.log('Auth data received:');
-      console.log(authData);
-      this.setState({
-        isFetching: false,
-        authData,
-      });
-    }).catch((error) => basicErrorHandling(error));
-  }
-
-  /**
-   * Renders the Login/Logout button. If the authorization data is still
-   * loading, the button is disabled.
-   * @return {React.Component}
-   * @override
-   */
-  render() {
-    let classList = 'btn btn-emphasis';
-    let linkText;
-    let authLink;
-    if (this.state.isFetching) {
-      // The button is disabled while the authorization data is being loaded.
-      classList += ' disabled';
-      linkText = 'Log In';
-      authLink = '/';
-    } else {
-      if (this.state.authData.authorized) {
-        if (this.state.authData.hasProfile) {
-          authLink = this.state.authData.logoutUrl;
-          linkText = 'Log Out';
-        } else {
-          authLink = USER_SIGNUP_URL;
-          linkText = 'Create Profile';
-        }
+export default function AuthButton(props) {
+  const authData = props.authData;
+  
+  let classList = 'btn btn-emphasis';
+  let linkText;
+  let authLink;
+  if (props.loading) {
+    // The button is disabled while the authorization data is being loaded.
+    classList += ' disabled';
+    linkText = 'Log In';
+    authLink = '/';
+  } else {
+    if (authData.authorized) {
+      if (authData.hasProfile) {
+        authLink = authData.logoutUrl;
+        linkText = 'Log Out';
       } else {
-        linkText = 'Log In';
-        authLink = this.state.authData.loginUrl;
+        authLink = USER_SIGNUP_URL;
+        linkText = 'Create Profile';
       }
+    } else {
+      linkText = 'Log In';
+      authLink = authData.loginUrl;
     }
-
-    return (
-      <a href={authLink} className={classList}>
-        {linkText}
-      </a>
-    );
   }
+
+  return (
+    <a href={authLink} className={classList}>
+      {linkText}
+    </a>
+  );
 }
+
+AuthButton.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  authData: authDataType,
+};
--- src/main/webapp/js/common/react/tests/Navbar.test.js
+++ src/main/webapp/js/common/react/tests/Navbar.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Navbar from '../components/Navbar.js';
 import '@testing-library/jest-dom/extend-expect';
 import {render, screen} from '@testing-library/react';
-import fetchMock from 'fetch-mock-jest';
-import {makeRelativeUrlAbsolute} from '../../../fetch_handler.js';
 
 const navbarUrls = [
   {
@@ -16,42 +14,21 @@ const navbarUrls = [
   },
 ];
 
-beforeAll(() => {
-  const authData = {
-    authorized: false,
-    loginUrl: '/login',
-    logoutUrl: '/logout',
-    hasProfile: false,
-  };
-
-  fetchMock.get(makeRelativeUrlAbsolute('/auth'), {
-    body: JSON.stringify(authData),
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-});
-
-afterAll(() => {
-  fetchMock.restore();
-});
-
 describe('Navbar component', () => {
   it('is added to the DOM', () => {
-    const elem = render(<Navbar urls={navbarUrls} />);
+    const elem = render(<Navbar urls={navbarUrls} loading={true} />);
     expect(elem.container.firstChild).not.toBeNull();
   });
 
   it('displays the names of the urls', () => {
-    render(<Navbar urls={navbarUrls} />);
+    render(<Navbar urls={navbarUrls} loading={true}  />);
 
     expect(screen.queryByText('Home')).not.toBeNull();
     expect(screen.queryByText('Projects')).not.toBeNull();
   });
 
   it('contains both urls', () => {
-    render(<Navbar urls={navbarUrls} />);
+    render(<Navbar urls={navbarUrls} loading={true} />);
 
     expect(screen.getByText('Home')).toHaveAttribute('href', '/');
     expect(screen.getByText('Projects'))
