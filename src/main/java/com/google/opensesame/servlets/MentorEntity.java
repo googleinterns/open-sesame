e297835b8e39db3b5d95f60ad26a15500cb9b49a
Sami Alves <50716380+Sami-2000@users.noreply.github.com>
2020-07-17 09:46:16 -0400

    Add Objectify dependency and create mentor and person entity classes. (#94)
    * Add Objectify dependency and create mentor and person entity classes.
    
    * Google Java Format
    
    * Update pom.xml
    
    Co-authored-by: Richie Goulazian <rgoulazian@google.com>
    
    Co-authored-by: github-actions <>
    Co-authored-by: Obi Abii <coa22@cornell.edu>
    Co-authored-by: Richie Goulazian <rgoulazian@google.com>

---
+++ src/main/java/com/google/opensesame/servlets/MentorEntity.java
@@ -0,0 +1,21 @@
+package com.google.opensesame.servlets;
+
+import com.googlecode.objectify.annotation.Subclass;
+import java.util.ArrayList;
+
+@Subclass(index = true)
+public class MentorEntity extends PersonEntity {
+  ArrayList<Long> projectIDs;
+  ArrayList<String> menteeIDs;
+
+  private MentorEntity() {
+    super();
+  }
+
+  public MentorEntity(
+      PersonEntity person, ArrayList<Long> projectIDs, ArrayList<String> menteeIDs) {
+    super(person.id, person.gitHubId, person.interestTags, person.email);
+    this.projectIDs = projectIDs;
+    this.menteeIDs = menteeIDs;
+  }
+}
