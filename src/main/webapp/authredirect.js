9f4213a5b969aa68c7cffcd5e3676465155b30c5
Richie Goulazian <rgoulazian@gmail.com>
2020-07-20 15:02:24 -0400

    Add temporary auth redirect

---
+++ src/main/webapp/authredirect.js
@@ -0,0 +1,19 @@
+fetch('/auth').then((response) => {
+  if (response.ok) {
+    return response.json()
+  } else {
+    return Promise.reject();
+  }
+}).then((authData) => {
+  if (authData.authorized) {
+    if (!authData.hasProfile) {
+      window.location.href = '/signup.html';
+    } else {
+      window.location.href = '/dashboard.html';
+    }
+  } else {
+    window.location.href = '/';
+  }
+}).catch((errorResponse) => {
+  window.location.href = '/';
+});
