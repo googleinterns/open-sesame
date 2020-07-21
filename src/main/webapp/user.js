e451589ed1fc054522c0ea435ee99156de5690c6
Obi Abii <obiabii@google.com>
2020-07-21 11:59:46 -0400

    remove unnecessary variables

--- src/main/webapp/user.js
+++ src/main/webapp/user.js
@@ -13,13 +13,11 @@ function getUser(userID = null) { //eslint-disable-line
   const fetchURL = '/user' + (userID ? '?userID=' + userID : '');
   const fetchRequest = fetch(makeRelativeUrlAbsolute(fetchURL));
 
-  const errorFormattedFetchRequest = standardizeFetchErrors(
-      fetchRequest,
-      'Failed to communicate with the server. Please try again later.',
-      'An error occcured while retrieving this account.' +
-    ' Please try again later.');
-
-  return errorFormattedFetchRequest.then((response) => response.json());
+  return standardizeFetchErrors(
+    fetchRequest,
+    'Failed to communicate with the server. Please try again later.',
+    'An error occcured while retrieving this account.' +
+    ' Please try again later.').then((response) => response.json());
 }
 
-export {getUser};
+export { getUser };
