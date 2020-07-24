5f0ad19327dcb220628ea58c07de2685494b1c7e
Sami Alves <scalves00@gmail.com>
2020-07-24 16:53:56 +0000

    Make homepage button dependent on auth status.

---
+++ src/main/webapp/js/common/react/index.js
@@ -0,0 +1,21 @@
+import {DataFetcher} from './components/DataFetcher.js';
+import {AuthButton} from './components/AuthButton.js';
+import {authDataFetch} from '../../auth_check.js';
+
+
+function renderButton(dataFetcher) {
+  return (
+    <AuthButton
+      loading={dataFetcher.isFetching}
+      className='btn btn-emphasis mt-5 mb-5 text-center btn-lg btn-block btn-main'
+      authData={dataFetcher.data} />
+  );
+}
+
+const buttonContainer = document.getElementById('button-container');
+ReactDOM.render(
+      <DataFetcher
+        createFetchRequest={() => authDataFetch}
+        render={renderButton} />,
+      buttonContainer);
+
