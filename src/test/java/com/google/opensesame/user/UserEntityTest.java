dbc44c32096f78cb685ab3a09fb3d93ac667835f
Obi Abii <coa22@cornell.edu>
2020-07-28 18:36:07 -0400

    Add tests for UserEntity (#146)
    * check that isMentor works
    
    * improve isMentor documentation
    
    * Google Java Format
    
    * swap assert arguments
    
    Co-authored-by: Bill Yeh <billyeh@google.com>
    
    Co-authored-by: github-actions <>
    Co-authored-by: Bill Yeh <billyeh@google.com>

---
+++ src/test/java/com/google/opensesame/user/UserEntityTest.java
@@ -0,0 +1,20 @@
+package com.google.opensesame.user;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UserEntityTest {
+  @Test
+  public void isMentorTest() throws IOException {
+    UserEntity mockUser = new UserEntity();
+    assertEquals(mockUser.isMentor(), false);
+
+    mockUser.addProject("12345L");
+    assertEquals(mockUser.isMentor(), true);
+  }
+}
