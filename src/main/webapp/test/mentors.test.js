b23cc54c59ddca453b896668f48b02de79d9847f
Sami-2000 <Sami-2000@users.noreply.github.com>
2020-07-28 15:03:28 +0000

    Apply ESLint changes.

--- src/main/webapp/test/mentors.test.js
+++ src/main/webapp/test/mentors.test.js
@@ -1,11 +1,11 @@
 import {getMentors, createMentorElement, createEmailLink, createLocationHeader, createImg, createBioParagraph, createNameHeader, createGitHubLink, createInterestTagsDiv} from '../mentors.js';
 import '@testing-library/jest-dom/extend-expect';
 
- global.mailtouiApp = {run: () => {}};
+global.mailtouiApp = {run: () => {}};
 
 const mockMentor = {
   interestTags: ['interest1', 'interest2'],
-  projects: [{respositoryID: 'mockRepoID', name: 'mockProjectName',}],
+  projects: [{respositoryID: 'mockRepoID', name: 'mockProjectName'}],
   bio: 'This is the mock bio.',
   email: 'samialves@google.com',
   gitHubID: 'mockGitID',
