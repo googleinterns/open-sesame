9d525d14e8437a7209785a3622c00c2e491c57e3
Richie Goulazian <rgoulazian@gmail.com>
2020-07-30 19:25:24 -0400

    Update testing for refactoring

--- src/test/java/com/google/opensesame/projects/ProjectQueryTest.java
+++ src/test/java/com/google/opensesame/projects/ProjectQueryTest.java
@@ -2,47 +2,39 @@ package com.google.opensesame.projects;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.opensesame.util.ServletValidationException;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.util.Closeable;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Collection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public class ProjectQueryTest {
-  private final LocalServiceTestHelper helper =
-      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+  private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
   private Closeable session;
 
   private ProjectEntity firstMockProject;
   private ProjectEntity secondMockProject;
   private ProjectEntity thirdMockProject;
-
-  private HttpServletResponse mockResponse;
-  private StringWriter responseStringWriter;
 
   @BeforeClass
   public static void objectifyClassSetup() {
@@ -58,20 +50,11 @@ public class ProjectQueryTest {
 
   @Before
   public void mockProjectSetUp() {
-    firstMockProject =
-        new ProjectEntity(
-            "273537467", Arrays.asList("Mentor 1", "Mentor 2", "Mentor 3"), Arrays.asList());
-    secondMockProject =
-        new ProjectEntity("45717250", Arrays.asList("Mentor 1", "Mentor 2"), Arrays.asList());
+    firstMockProject = new ProjectEntity("273537467", Arrays.asList("Mentor 1", "Mentor 2", "Mentor 3"),
+        Arrays.asList());
+    secondMockProject = new ProjectEntity("45717250", Arrays.asList("Mentor 1", "Mentor 2"), Arrays.asList());
     thirdMockProject = new ProjectEntity("20580498", Arrays.asList("Mentor 1"), Arrays.asList());
     ofy().save().entities(firstMockProject, secondMockProject, thirdMockProject).now();
-  }
-
-  @Before
-  public void mockResponseSetUp() throws IOException {
-    mockResponse = mock(HttpServletResponse.class);
-    responseStringWriter = new StringWriter();
-    when(mockResponse.getWriter()).thenReturn(new PrintWriter(responseStringWriter));
   }
 
   @After
@@ -81,32 +64,31 @@ public class ProjectQueryTest {
   }
 
   @Test
-  public void queryByIdsShouldReturnProjectsWithThoseIds() throws IOException {
+  public void queryByIdsShouldReturnProjectsWithThoseIds()
+      throws IOException, ServletValidationException {
     // Expect to receive the ProjectEntities with the corresponding repository IDs.
 
     HttpServletRequest request = mock(HttpServletRequest.class);
     when(request.getParameterValues(ProjectEntity.PROJECT_ID_PARAM))
         .thenReturn(new String[] {firstMockProject.repositoryId, secondMockProject.repositoryId});
 
-    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request, mockResponse);
+    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request);
     Collection<ProjectEntity> expected = Arrays.asList(firstMockProject, secondMockProject);
 
     assertNotNull(queryResult);
     assertTrue(queryResult.size() == expected.size() && queryResult.containsAll(expected));
-    verify(mockResponse, never()).setStatus(anyInt());
   }
 
   @Test
-  public void queryWithNoIdsShouldReturnEmpty() throws IOException {
+  public void queryWithNoIdsShouldReturnEmpty() throws IOException, ServletValidationException {
     // Expect to receive no ProjectEntities because the list of IDs was defined but empty.
 
     HttpServletRequest request = mock(HttpServletRequest.class);
     when(request.getParameterValues(ProjectEntity.PROJECT_ID_PARAM)).thenReturn(new String[] {});
 
-    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request, mockResponse);
+    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request);
     assertNotNull(queryResult);
     assertTrue(queryResult.size() == 0);
-    verify(mockResponse, never()).setStatus(anyInt());
   }
 
   @Test
@@ -118,19 +100,20 @@ public class ProjectQueryTest {
     when(request.getParameterValues(ProjectEntity.PROJECT_ID_PARAM))
         .thenReturn(new String[] {firstMockProject.repositoryId, "invalidID"});
 
-    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request, mockResponse);
-    assertNull(queryResult);
-    verify(mockResponse, times(1)).setStatus(HttpServletResponse.SC_NOT_FOUND);
+    ServletValidationException validationException = Assert.assertThrows(
+        ServletValidationException.class,
+        () -> ProjectQuery.queryFromRequest(request));
+    assertEquals(HttpServletResponse.SC_NOT_FOUND, validationException.getStatusCode());
   }
 
   @Test
-  public void queryWithoutIdsShouldReturnAllProjects() throws IOException {
+  public void queryWithoutIdsShouldReturnAllProjects() throws IOException, ServletValidationException {
     // Expect to receive all ProjectEntities in order of descending number of mentors because no
     // list of IDs was supplied.
 
     HttpServletRequest request = mock(HttpServletRequest.class);
 
-    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request, mockResponse);
+    Collection<ProjectEntity> queryResult = ProjectQuery.queryFromRequest(request);
     ProjectEntity[] expectedQueryResult =
         new ProjectEntity[] {firstMockProject, secondMockProject, thirdMockProject};
     assertNotNull(queryResult);
--- src/test/java/com/google/opensesame/projects/QueryFilterTest.java
+++ src/test/java/com/google/opensesame/projects/QueryFilterTest.java
@@ -2,36 +2,20 @@ package com.google.opensesame.projects;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+import com.google.opensesame.util.ServletValidationException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.http.HttpServletResponse;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public class QueryFilterTest {
-  private HttpServletResponse mockResponse;
-  private StringWriter responseStringWriter;
-
-  @Before
-  public void mockResponseSetUp() throws IOException {
-    mockResponse = mock(HttpServletResponse.class);
-    responseStringWriter = new StringWriter();
-    when(mockResponse.getWriter()).thenReturn(new PrintWriter(responseStringWriter));
-  }
-
   @Test
-  public void parseQueryFilter() throws IOException {
-    QueryFilter queryFilter = QueryFilter.fromString("numMentors > 123", mockResponse);
+  public void parseQueryFilter() throws IOException, ServletValidationException {
+    QueryFilter queryFilter = QueryFilter.fromString("numMentors > 123");
 
     assertNotNull(queryFilter);
     assertEquals("numMentors >", queryFilter.condition);
@@ -40,35 +24,37 @@ public class QueryFilterTest {
 
   @Test
   public void handlesInvalidString1() throws IOException {
-    QueryFilter queryFilter = QueryFilter.fromString("numMentors> 123", mockResponse);
+    ServletValidationException validationException = Assert.assertThrows(
+        ServletValidationException.class, 
+        () -> QueryFilter.fromString("numMentors> 123"));
 
-    assertNull(queryFilter);
-    verify(mockResponse, times(1)).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, validationException.getStatusCode());
   }
 
   @Test
   public void handlesInvalidString2() throws IOException {
-    QueryFilter queryFilter = QueryFilter.fromString("numMentors => 123", mockResponse);
+    ServletValidationException validationException = Assert.assertThrows(
+        ServletValidationException.class, 
+        () -> QueryFilter.fromString("numMentors => 123"));
 
-    assertNull(queryFilter);
-    verify(mockResponse, times(1)).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, validationException.getStatusCode());
   }
 
   @Test
   public void handlesInvalidField() throws IOException {
-    QueryFilter queryFilter =
-        QueryFilter.fromString("notAFieldInProjectEntity > 123", mockResponse);
+    ServletValidationException validationException = Assert.assertThrows(
+        ServletValidationException.class, 
+        () -> QueryFilter.fromString("notAFieldInProjectEntity > 123"));
 
-    assertNull(queryFilter);
-    verify(mockResponse, times(1)).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, validationException.getStatusCode());
   }
 
   @Test
   public void handlesInvalidComparisonValue() throws IOException {
-    QueryFilter queryFilter =
-        QueryFilter.fromString("numMentors > wontConvertToNumMentorsFieldType", mockResponse);
+    ServletValidationException validationException = Assert.assertThrows(
+        ServletValidationException.class, 
+        () -> QueryFilter.fromString("numMentors > wontConvertToNumMentorsFieldType"));
 
-    assertNull(queryFilter);
-    verify(mockResponse, times(1)).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, validationException.getStatusCode());
   }
 }
