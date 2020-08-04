616b158e911c7aa7b0574658e0493e5adf840065
Richie Goulazian <rgoulazian@google.com>
2020-08-04 13:21:03 -0400

    Add backend support for filters to project queries from servlet. (#151)
    * Add testing dependencies
    
    * Add project entity query tests
    
    * Add tests for from repository
    
    * Google Java Format
    
    * Add reflection for queryable field names
    
    * Move to project query
    
    * Update project backend tests
    
    * Add filter query
    
    * Move to project query
    
    * Expose queryable fields to make testing easier
    
    * Remove field param
    
    * Remove unused import
    
    * Add tests for parsing filter query
    
    * Rename to QueryFilter
    
    * Fix naming in tests
    
    * Integrate filters into project query
    
    * Google Java Format
    
    * Style edits to ProjectQuery
    
    * Style changes and documentation improvements
    
    * Google Java Format
    
    * Fix project entity test
    
    * Google Java Format
    
    * Remove "Please try again later."
    
    * Move method back to ProjectEntity
    
    * Google Java Format
    
    * Refactor to use exceptions
    
    * Update testing for refactoring
    
    * Google Java Format
    
    * Fix multiple filters
    
    * Remove premature projection optimization
    
    * Google Java Format
    
    * Improve testing
    
    * Google Java Format
    
    * Update filter regex
    
    * Escape new backslash
    
    * Move filter query regex to function scope
    
    * Update documentation to match switch to exceptions
    
    * Restructure variable declarations
    
    * Refactor query filter from string
    
    * Refactored queryFromRequest
    
    * Google Java Format
    
    * Remove FILTER_QUERY_PARAM constant
    
    * Improve the naming of the comparison value object
    
    * Google Java Format
    
    * Remove test depedence on removed constant
    
    * Google Java Format
    
    Co-authored-by: github-actions <>

--- src/main/java/com/google/opensesame/servlets/ProjectPreviewServlet.java
+++ src/main/java/com/google/opensesame/servlets/ProjectPreviewServlet.java
@@ -3,6 +3,9 @@ package com.google.opensesame.servlets;
 import com.google.gson.Gson;
 import com.google.opensesame.projects.ProjectData;
 import com.google.opensesame.projects.ProjectEntity;
+import com.google.opensesame.projects.ProjectQuery;
+import com.google.opensesame.util.ErrorResponse;
+import com.google.opensesame.util.ServletValidationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -38,7 +41,13 @@ public class ProjectPreviewServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Collection<ProjectEntity> projectEntities = ProjectEntity.queryFromRequest(request, response);
+    Collection<ProjectEntity> projectEntities;
+    try {
+      projectEntities = ProjectQuery.queryFromRequest(request);
+    } catch (ServletValidationException e) {
+      ErrorResponse.sendJsonError(response, e);
+      return;
+    }
 
     if (projectEntities != null) {
       List<ProjectData> projectData = new ArrayList<ProjectData>();
